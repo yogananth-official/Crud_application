1d4b6e7972d0972767bb0226e5f198cb1112f648
yogananth <yogananthofficial@gmail.com>
2025-03-18 20:01:50 +0530

    made changes in index.jsx

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const UserModel = require('./models/Users');
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
