bb1776951749e6db9c15ef30468c258c9cfd39c9
yogananth <yogananthofficial@gmail.com>
2025-03-18 19:50:04 +0530

    Removed node_modules from tracking

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Users from "./Users";
+import CreateUser from "./CreateUser"; 
+import UpdateUser from "./UpdateUser"; 
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update/:id" element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud");
+
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete(id)
+        .then(user => res.json({ message: "User deleted successfully", user }))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
+
+
+app.get('/', (req, res) => {
+    UserModel.find({})
+        .then(users => res.json(users))
+        .catch(err => res.json(err));
+});
+
+app.get('/getUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById(id)
+        .then(user => res.json(user))
+        .catch(err => res.json(err));
+});
+
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+        .then(user => res.json(user))
+        .catch(err => res.json(err));
+});
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate(id, req.body, { new: true })
+        .then(user => res.json(user))
+        .catch(err => res.json(err));
+});
+
+app.listen(3001, () => {
+    console.log("server is running");
+});
